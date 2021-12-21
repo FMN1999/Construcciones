6b0752454a0a0f7401dfc4f428a3ef8900891ddf
Biscaldi-Iv <Biscaldi-Iv>
2021-12-21 15:31:52 -0300

    implementacion POO en materiales

--- src/main/java/logica/MaterialLogic.java
+++ src/main/java/logica/MaterialLogic.java
@@ -5,28 +5,36 @@ import java.util.ArrayList;
 
 import datos.MaterialData;
 import entidades.Material;
+import entidades.Proveedor;
 
 public class MaterialLogic {
 	public static MaterialData source=new MaterialData();
 	
 	//no se captura ningun error, todos van a capa de presentacion
 	
-	public static void ActualizarDatos(Material m) throws SQLException, Exception {
+	public static void ActualizarDatos(Material m, int id_prov) throws SQLException, Exception {
 		boolean modificaPre=false;
 		Material mVerif=source.getOne(m.getId_material());
 		if(mVerif.getPrecio()-m.getPrecio()!=0) {
 			modificaPre=true;
 		}
-		source.ActualizarDatos(m, modificaPre);
+		source.ActualizarDatos(m, modificaPre, id_prov);
 	}
-	
-	public static ArrayList<Material> getAll() throws Exception{
+	//reemplazar
+	/*public static ArrayList<Material> getAll() throws Exception{
 		ArrayList<Material> mats=source.getAll();
 		return mats;
+	}*/
+	public static ArrayList<Proveedor> getAll(ArrayList<Proveedor> provs) throws Exception{
+		for(Proveedor prov:provs) {
+			prov.setMateriales(source.getMateriales(prov.getIdProveedor()));
+		}
+		
+		return provs;
 	}
 	
-	public static void Registrar(Material m) throws Exception {
-		source.Registrar(m);
+	public static void Registrar(Material m, int id_prov) throws Exception {
+		source.Registrar(m, id_prov);
 	}
 	
 	public static void Eliminar(int id) throws Exception{
