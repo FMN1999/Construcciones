c6980540f73b4bd2bd9d49b5ee0a654f9bad06ca
Matias <santolarimatias@gmail.com>
2022-02-20 18:01:58 -0300

    agregado get maquinarias y get materiales a presupuesto data

--- src/main/java/datos/PresupuestoData.java
+++ src/main/java/datos/PresupuestoData.java
@@ -1,3 +1,4 @@
+
 package datos;
 
 import java.sql.PreparedStatement;
@@ -5,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import entidades.Maquinaria;
+import entidades.Material;
 import entidades.Obra;
 import entidades.Presupuesto;
 
@@ -14,7 +17,7 @@ public class PresupuestoData extends Coneccion {
 		ArrayList<Presupuesto> presups=new ArrayList<Presupuesto>();
 		try {
 			this.open();
-			PreparedStatement ps=this.getCon().prepareStatement("SELECT p.idpresupuesto, p.fecha_emision, p.monto FROM presupuestos p WHERE o.id_obra=? ");
+			PreparedStatement ps=this.getCon().prepareStatement("SELECT p.idpresupuesto, p.fecha_emision, p.monto FROM presupuestos p WHERE p.id_obra=? ");
 			ps.setInt(1, o.getIdObra());
 			ResultSet rs=ps.executeQuery();
 			while(rs.next()) {
@@ -30,4 +33,58 @@ public class PresupuestoData extends Coneccion {
 		}
 		return presups;
 	}
+	
+	public ArrayList<Material> getMateriales(Presupuesto p) throws SQLException{
+		ArrayList<Material> ms=new ArrayList<Material>();
+		try {
+			this.open();
+			PreparedStatement ps=this.getCon().prepareStatement("SELECT p.idpresupuesto, t.idtarea, m.idmaterial, m.descripcion, mt.cant_a_usar, pm.precio \r\n"
+					+ "from presupuestos p\r\n"
+					+ "inner join tareas t on t.id_presupuesto=p.idpresupuesto\r\n"
+					+ "inner join materiales_tareas mt on mt.id_tarea_=t.idtarea\r\n"
+					+ "inner join materiales m on m.idmaterial=mt.id_material_\r\n"
+					+ "inner join precios_material pm on m.idmaterial=pm.id_material\r\n"
+					+ "where p.idpresupuesto=? ");
+			ps.setInt(1, p.getId_presupuesto());
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				ms.add(new Material(rs.getInt("m.idmaterial"), rs.getString("m.descripcion"), rs.getFloat("pm.precio")));
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			throw e;
+		}
+		finally {
+			this.close();
+		}
+		return ms;
+	}
+	
+	public ArrayList<Maquinaria> getMaquinarias(Presupuesto p) throws SQLException{
+		ArrayList<Maquinaria> ms=new ArrayList<Maquinaria>();
+		try {
+			this.open();
+			PreparedStatement ps=this.getCon().prepareStatement("SELECT p.idpresupuesto, t.idtarea, m.idmaquina, m.descripcion, m.precioHora\r\n"
+					+ "from presupuestos p\r\n"
+					+ "inner join tareas t on t.id_presupuesto=p.idpresupuesto\r\n"
+					+ "inner join tareas_maquinas tm on tm.id_tarea__=t.idtarea\r\n"
+					+ "inner join maquinarias m on m.idmaquina=mt.id_maquina__\r\n"
+					+ "where p.idpresupuesto=? ");
+			ps.setInt(1, p.getId_presupuesto());
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				ms.add(new Maquinaria(rs.getInt("m.idmaquina"), rs.getString("m.descripcion"), rs.getFloat("m.precioHora")));
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			throw e;
+		}
+		finally {
+			this.close();
+		}
+		return ms;
+	}
 }
+	
