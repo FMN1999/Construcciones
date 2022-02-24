b09f6b822824c41eac8d15d0a2a628e6ade57da9
Biscaldi-Iv <ivanbisc12@gmail.com>
2022-02-24 00:38:30 -0300

    Clase para envio de mails

---
+++ src/main/java/mail_magement/mail_util.java
@@ -0,0 +1,59 @@
+package mail_magement;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class mail_util {
+	public static void Send(String receptor,String asunto,String texto) throws Exception {
+		System.out.println("Iniciando envio de email");
+		Properties info=new Properties();
+		
+		info.put("mail.smtp.auth", "true");
+		info.put("mail.smtp.starttls.enable", "true");
+		info.put("mail.smtp.host", "smtp.gmail.com");
+		info.put("mail.smtp.port", "587");
+		
+		String myemail="construcciones.java.utn@gmail.com";
+		String password="";
+		
+		Session session= Session.getInstance(info, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(myemail,password);
+			}
+		});
+		
+		try {
+			Message msg= prepareMessage(session, myemail, receptor, asunto, texto);
+			Transport.send(msg);
+			System.out.println("Enviado!!!");
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			throw new Exception("Address Exception"+e.getMessage());
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			throw new Exception("Messaging Exception"+e.getMessage());
+		}
+		
+		
+	}
+
+	private static Message prepareMessage(Session s, String myemail,String receptor, String asunto, String texto) throws AddressException, MessagingException {
+		// TODO Auto-generated method stub
+		Message m=new MimeMessage(s);
+		m.setFrom(new InternetAddress(myemail));
+		m.setRecipient(Message.RecipientType.TO, new InternetAddress(receptor));
+		m.setSubject(asunto);
+		m.setText(texto);
+		return m;
+	}
+}
